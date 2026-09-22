3618885e0fba8952a3fce5cf8f97f76b16482f7b
JohnYook <45766356+JohnYook@users.noreply.github.com>
2026-09-22 11:49:56 -0400

    Allow run script to pass in input file

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-python3 src/main.py transactions.csv
+if [ "$#" -gt 1 ]; then
+    echo "Usage: $0 <input_file>"
+    exit 1
+elif [ "$#" -lt 1 ]; then
+    input_file="transactions.csv"
+else
+    input_file="$1"
+fi
+
+python3 src/main.py $input_file
